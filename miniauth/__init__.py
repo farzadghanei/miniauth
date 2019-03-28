9a41ab17ab9491243f7309809b4b173265fbf01b
Farzad Ghanei <farzad.ghanei@gmail.com>
2019-03-28 22:59:06 +0100

    bump version to 0.2.0

--- miniauth/__init__.py
+++ miniauth/__init__.py
@@ -6,4 +6,4 @@ Simple program and library for local user authentication.
 :license: This software is released under the terms of MIT license.
           See LICENSE file for more details.
 """
-__version__ = '0.1.0'
+__version__ = '0.2.0'
